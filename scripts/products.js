3031412240c67e2d48c0a5092241df6bcb43b01a
‘Katy’ <katyinseattle@me.com>
2017-09-10 15:41:51 -0700

    things

---
+++ scripts/products.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var products = {};
+
+products.populateFilters = function() {
+  $('product').each(function() {
+    if (!$(this).hasClass('template')) {
+      var val = $(this).find('')
+    }
+  }
+)
+}
