461d09990b26200b078aef6b11e7db2cfc195c94
‘Katy’ <katyinseattle@me.com>
2017-09-11 13:25:23 -0700

    ugh

--- server.js
+++ server.js
@@ -1,34 +1,14 @@
-'use strict';
-const pg = require('pg');
-const fs = require('fs');
-const express = require('express');
-const bodyParser = require('body-parser');
+const http = require('http');
 
-const app = express ();
-const conString = 'postgres://postgres:1234@localhost:5432/lab10'; 
-const client = new pg.Client(conString);
-client.connect();
-client.on('error', err => console.error(err));
+const hostname = "127.0.0.1"
+const port = 8080;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.get('/', function (req, res){
-res.sendFile(__dirname + '/index.html');
-
+const server = createServer ((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
 });
 
-app.use(express.static(__dirname + '/public'));
-app.use('/public',express.static('public'))
-
-
-
-const PORT = process.env.PORT || 3000;
-
-app.post('.data', function (req, res) {
-  req.on('data', function (data){
-  console.log(data.toString());
-  })
-  req.on ('end', function() {
-    res.send('data received!');
-  })
-})
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
