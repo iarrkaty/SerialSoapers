bb2af524e18508af6412517bde0f781ba0d824d0
‘Katy’ <katyinseattle@me.com>
2017-09-12 08:26:48 -0700

    trying

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const http = require('http');
-
-const hostname = "127.0.0.1"
-const port = 8080;
-
-const server = createServer ((req, res) => {
+'use strict';
+const express = require('express');
+const app = express();
+app.use((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('App is running on port', port);
 });
