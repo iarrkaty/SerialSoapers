7507a2c89e32b5e1e3b42ffe335a1e913e49b661
‘Katy’ <katyinseattle@me.com>
2017-09-13 08:30:27 -0700

    guh

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 app.use((req, res) => {
   res.statusCode = 200;
-});
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.get('/', function (req, res){
-res.sendFile(__dirname + '/index.html');
+  res.setHeader('Content-Type', 'text/plain');
 
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.get('/', function (req, res){
+  res.sendFile('/index.html');
 });
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
